3a6b6e7128900345d057196e20a9d83809f77f44
Wolf <coff3programing@gmail.com>
2024-02-12 10:57:53 -0500

    First Section concluid

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+admin.site.register(Category)
+# admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['category', 'name', 'description' , 'price', 'image']
+    list_display = ['name', 'category', 'price', 'image', 'register_date']
--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    register_date = models.DateField(auto_now_add=True)
+    
+    class Meta:
+        db_table = 'Categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+      
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    register_date = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to='products', blank=True)
+    
+    class Meta:
+        db_table = 'Products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
